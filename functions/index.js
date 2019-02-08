9ac424824867e6737eb1194602a8573520e32be2
danalboteanu <alboteanud@gmail.com>
2019-02-08 21:06:42 +0200

    ok

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 // @ts-check
 const functions = require('firebase-functions')
 var admin = require("firebase-admin")
+
 var serviceAccount = require("./serviceAccountKey.json")
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
@@ -20,79 +21,126 @@ exports.onCreateUser = functions.auth.user().onCreate(firebaseUser => {
     return admin.firestore().doc(`users/${uid}`).set(user)
 })
 
+// listen for new Conversations and set up the User data
+exports.onCreateConversation = functions.firestore.document('users/{uid}/conversations/{uidP}').onCreate((snap, context) => {
+    const uidP = context.params.uidP
+    const uid = context.params.uid
+
+    return admin.firestore().doc(`users/${uidP}`).get()
+        .then(doc => {
+            if (doc.exists) {
+                const user = doc.data()
+                const palData = {
+                    name: user.name,
+                    uid: user.uid,
+                    email: user.email,
+                    photoUrl: user.photoUrl
+                }
+                // set up conversation User data
+                return snap.ref.update(palData)
+            } else {
+                // doc.data() will be undefined in this case
+                console.log("No such document!");
+                return null
+            }
+        }).catch(error => console.log("Error getting document:", error))
+})
+
 exports.onNewSound = functions.storage.object().onFinalize(object => {
+      if (!object.contentType.startsWith('audio/3gpp')) return null;
+
     const fileBucket = object.bucket; // The Storage bucket that contains the file.
     const filePath = object.name; // File path in the bucket.
-    const contentType = object.contentType; // File content type.
     const metageneration = object.metageneration; // Number of times metadata has been generated. New objects have a value of 1.
 
-    if (!contentType.startsWith('video/')) return 0
 
     const pathArray = filePath.split('/')
     const uid = pathArray[2]
     const uidP = pathArray[3]
 
+    const refConv = admin.firestore().doc(`users/${uid}/conversations/${uidP}`)
     const refConvPal = admin.firestore().doc(`users/${uidP}/conversations/${uid}`)
-    return refConvPal.collection('settings').doc('blocked').get().then(docBlk => {
-        if (docBlk.exists) return 0 // usr is blocked
 
-        const bucket = admin.storage().bucket(fileBucket)
-        const file = bucket.file(filePath)
-        return file.getSignedUrl({
-            action: 'read',
-            expires: '01-01-2091'
-        }).then(signedUrls => {
-            return notifyUserNewSound(uid, uidP, signedUrls[0])
+    return refConvPal.collection('settings').doc('blocked').get()
+        .then(docBlk => {
+            if (docBlk.exists) return null;     // the user is blocked
+
+            const bucket = admin.storage().bucket(fileBucket)
+            return bucket.file(filePath).getSignedUrl({ action: 'read', expires: '01-01-2091' })
+        }).then(urls => {
+            const now = Date.now()
+            // update conversation status wits sound details
+            const data = {
+                soundUrl: urls[0],
+                soundTime: now,
+                timestampModif: now
+            }
+
+            notifyUserNewSound(uid, uidP, urls[0])
+
+            const promises = []
+            promises.push(refConv.set(data, { merge: true }))
+            promises.push(refConvPal.set(data, { merge: true }))
+            return Promise.all(promises)
         })
-    })
 })
 
-// updating PAL - new msg and Conversation update
+// updating PAL with new msg and Conversation update
 exports.onNewTextMsg = functions.firestore.document('users/{uid}/conversations/{uidP}/messages/{msgId}').onCreate((snap, context) => {
     const message = snap.data()
     const uid = context.params.uid
-    if (message.uid != uid) return 0
-
     const uidP = context.params.uidP
     const msgId = context.params.msgId
-    const refConvPal = admin.firestore().doc(`users/${uidP}/conversations/${uid}`)
-    const refMsg = refConvPal.collection("messages").doc(`${msgId}`)
 
-    return refConvPal.collection('settings').doc('blocked').get().then(docBlk => {
-        if (docBlk.exists) return 0 // usr is blocked
+    if (message.uid === uid) {  // making sure this user sent the msg
 
-        const data = {
-            msgText: message.msgText,
-            msgAuthor: message.msgAuthor,
-            msgTimestamp: message.msgTimestamp
-        }
+        const refConvPal = admin.firestore().doc(`users/${uidP}/conversations/${uid}`)
+        const refMsg = refConvPal.collection("messages").doc(`${msgId}`)
 
-        const promises = []
-        promises.push(refMsg.set(message, { merge: true }))
-        promises.push(refConvPal.set(data, { merge: true }))
-        return Promise.all(promises)
-    })
+        return refConvPal.collection('settings').doc('blocked').get().then(docBlk => {
+            if (docBlk.exists) return null;     // the user is blocked so he cannot update pal data
+
+            // update the conversation status with msg details
+            const data = {
+                msgText: message.msgText,
+                msgAuthor: message.msgAuthor,
+                msgTimestamp: message.msgTimestamp,
+                timestampModif: message.timestampModif
+            }
+
+            const promises = []
+            promises.push(refMsg.set(message, { merge: true }))
+            promises.push(refConvPal.set(data, { merge: true }))
+            return Promise.all(promises)
+        })
+    } else {  // this user just received a new msg from pal (above - if)
+
+        return notifyUserNewTextMsg(uid, message)
+    }
 })
 
-// listen for new Conversations and set user data
-exports.onCreateConversation = functions.firestore.document('users/{uid}/conversations/{uidP}').onCreate((snap, context) => {
-    const uidP = context.params.uidP
-    const uid = context.params.uid
-    return admin.firestore().doc(`users/${uidP}`).get().then(doc => {
-        const user = doc.data()
-        const palData = {
-            name: user.name,
-            uid: user.uid,
-            email: user.email,
-            photoUrl: user.photoUrl,
+function notifyUserNewTextMsg(uid, message) {
+    return admin.firestore().collection(`users/${uid}/tokens`).get().then(snapshot => {
+        let tokens = []
+        snapshot.forEach(doc => tokens.push(doc.id))
+
+        if (tokens.length === 0) return null
+
+        const payload = {
+            data: {
+                uidP: message.uid,
+                msgText: message.msgText,
+                msgAuthor: message.msgAuthor,
+                photoUrl: message.photoUrl
+            }
         }
-        // set conversation UserData
-        return snap.ref.update(palData)
+        const options = { priority: "high" }
+        return admin.messaging().sendToDevice(tokens, payload, options)
     })
-})
+}
 
 function notifyUserNewSound(uid, uidP, soundUrl) {
-    admin.firestore().collection(`users/${uidP}/tokens`).get().then(snapshot => {
+    return admin.firestore().collection(`users/${uidP}/tokens`).get().then(snapshot => {
         let tokens = []
         snapshot.forEach(doc => tokens.push(doc.id))
         const payload = {
@@ -155,3 +203,5 @@ function cleanupTokens(response, tokens) {
     })
     return Promise.all(tokensDelete);
 }
+
+
